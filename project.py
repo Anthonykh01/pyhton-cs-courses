9e60a7a7fcb141ee1aab830a174331ad6578ad1b
anthonyykh <anthonykh01.ak@gmail.com>
2023-05-08 20:32:25 +0300

    login

--- project.py
+++ project.py
@@ -1,28 +1,33 @@
+import os
 import sys
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
-def login_to_portal(username, password):
-    LOGIN_URL = "https://myportal.lau.edu.lb/"
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
+def fetch_element(driver_instance, search_by, value):
+    return driver_instance.find_element(search_by, value)
 
-    driver.get(LOGIN_URL)
+def fetch_course_information():
+    credentials = {
+        'username': sys.argv[1],
+        'password': sys.argv[2]
+    }
 
-    username_input = driver.find_element(By.NAME, "username")
-    username_input.send_keys(username)
+    LAU_PORTAL = "https://myportal.lau.edu.lb/"
+    COURSE_URL = "https://banweb.lau.edu.lb/prod/bwckschd.p_get_crse_unsec"
 
-    password_input = driver.find_element(By.NAME, "password")
-    password_input.send_keys(password)
+    chrome_config = webdriver.ChromeOptions()
+    chrome_config.add_argument("--headless")
+    driver_instance = webdriver.Chrome(options=chrome_config)
 
-    login_button = driver.find_element(By.XPATH, "//input[@type='submit']")
-    login_button.click()
+    driver_instance.get(LAU_PORTAL)
 
-    return driver
+    fetch_element(driver_instance, By.NAME, "username").send_keys(credentials['username'])
+    fetch_element(driver_instance, By.NAME, "password").send_keys(credentials['password'])
+    fetch_element(driver_instance, By.XPATH, "//input[@type='submit']").click()
 
-username = sys.argv[1]
-password = sys.argv[2]
+ 
 
-driver = login_to_portal(username, password)
-
+if __name__ == "__main__":
+    fetch_course_information()
